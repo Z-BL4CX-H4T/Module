68e698a2f9cafcd0d26af1662bf278f3fbfa3878
Z-BL4CX-H4T <123639062+Z-BL4CX-H4T@users.noreply.github.com>
2025-09-25 11:53:40 +0800

    Update Module-Linux.py

--- Module-Linux.py
+++ Module-Linux.py
@@ -1,59 +1,96 @@
-# module
-from os import system
-from time import sleep
+# Module-Linux.py
+# Tools Installer Linux Version
+# Author : Z-SH4DOWSPEECH
 
-# program
+import os
+import sys
+import time
+import itertools
+
+__author__ = "Z-SH4DOWSPEECH"
+__version__ = "1.0.0"
+
+# === LOGO ===
 def logo():
-   print("""
-
-\33[5;32m __________ _____ _   _ __  __  ___  ____  _   _ _     _____\33[5;32m
-\33[5;32m|__  / ____|_   _| | | |  \/  |/ _ \|  _ \| | | | |   | ____|\33[5;32m
-  \33[5;32m/ /|  _|   | | | |_| | |\/| | | | | | | | | | | |   |  _|\33[5;32m
- \33[5;32m/ /_| |___  | | |  _  | |  | | |_| | |_| | |_| | |___| |___ \33[5;32m
-\33[5;32m/____|_____| |_| |_| |_|_|  |_|\___/|____/ \___/|_____|_____|\33[5;32m  
+    print("""
+\033[5;32m
+  __  __           _       _            _     _                  
+ |  \/  | ___   __| |_   _| | ___      | |   (_)_ __  _   ___  __
+ | |\/| |/ _ \ / _` | | | | |/ _ \_____| |   | | '_ \| | | \ \/ /
+ | |  | | (_) | (_| | |_| | |  __/_____| |___| | | | | |_| |>  < 
+ |_|  |_|\___/ \__,_|\__,_|_|\___|     |_____|_|_| |_|\__,_/_/\_\
+\033[0m
 """)
 
-logo()
-def menu():
-   system("""\33[5;31mapt update\33[5;31m
-apt upgrade
-apt install python
-apt install python2
-apt install curl
-apt install tar
-apt install zip
-apt install unzip
-apt install tor
-apt install wget
-apt install wcalc
-apt install bmon
-apt install golang
-apt install perl
-apt install openssl
-apt install openssh
-apt install zsh
-apt install fortune
-apt install htop
-apt install dnsutils
-apt install cmatrix
-apt install ranger
-apt install libcaca
-apt install git
-apt install toilet
-apt install neofetch
-apt install php
-apt install pv
-apt install w3m 
-apt install sl
-\33[5;31m""")
-   print("""
+# === ANIMASI SPINNER ===
+def spinner(task, duration=3):
+    spinner_cycle = itertools.cycle(['|', '/', '-', '\\'])
+    start_time = time.time()
+    while (time.time() - start_time) < duration:
+        sys.stdout.write(f"\r\033[94m{task}... {next(spinner_cycle)}\033[0m")
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write("\r\033[92m[SUCCESS]\033[0m\n")
 
-\33[0;35m ____  _____ _     _____ ____    _    ___\33[0;35m 
-\33[0;35m/ ___|| ____| |   | ____/ ___|  / \  |_ _|\33[0;35m
-\33[0;35m\___ \|  _| | |   |  _| \___ \ / _ \  | |\33[0;35m
-\33[0;35m ___) | |___| |___| |___ ___) / ___ \ | |\33[0;35m
-\33[0;35m|____/|_____|_____|_____|____/_/   \_\___|\33[0;35m
+# === ANIMASI PROGRESS BAR ===
+def progress_bar(task, steps=30):
+    for i in range(steps + 1):
+        bar = "█" * i + "-" * (steps - i)
+        sys.stdout.write(f"\r\033[96m{task} |{bar}| {int((i/steps)*100)}%\033[0m")
+        sys.stdout.flush()
+        time.sleep(0.05)
+    sys.stdout.write("\n")
+
+# === INSTALLER ===
+def install_packages():
+    packages = [
+        "update && sudo apt upgrade -y",
+        "install perl -y",
+        "install python3 -y",
+        "install curl -y",
+        "install tar -y",
+        "install zip -y",
+        "install unzip -y",
+        "install tor -y",
+        "install golang -y",
+        "install openssl -y",
+        "install openssh-client -y",
+        "install vim -y",
+        "install zsh -y",
+        "install htop -y",
+        "install dnsutils -y",
+        "install cmatrix -y",
+        "install ranger -y",
+        "install git -y",
+        "install neofetch -y",
+        "install toilet -y",
+        "install php -y",
+        "install lolcat -y",
+        "install pv -y",
+        "install w3m -y"
+    ]
+
+    for pkg in packages:
+        print(f"\033[5;31m>> Installing: apt {pkg}\033[0m")
+        spinner("Downloading")      # animasi spinner
+        progress_bar("Installing")  # progress bar smooth
+        os.system(f"sudo apt {pkg}")
+        print()
+
+# === MAIN RUN ===
+def run():
+    logo()
+    install_packages()
+    print("""
+\033[0;33m
+  _                 _ 
+ ___  ___| | ___  ___  __ _(_)
+ / __|/ _ \\ |/ _ \\/ __|/ _` | |
+ \\__ \\  __/ |  __/\\__ \\ (_| | |
+ |___/\\___|_|\\___||___/\\__,_|_|
+\033[0m
 """)
-   sleep(1)
+    print(f"\033[1;36mTools created by: {__author__}\033[0m\n")
 
-menu()
+if __name__ == "__main__":
+    run()
